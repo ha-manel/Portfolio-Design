52eb4fec1a7c58926df8c464044b3af94073ae90
Manel <manelhammouche3@gmail.com>
2022-04-04 09:23:25 +0100

    Fix linter errors

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const hamburger= document.querySelector("#hamburger");
-const navBar = document.querySelector("#mobile-navbar");
+const hamburger = document.querySelector('#hamburger');
+const navBar = document.querySelector('#mobile-navbar');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
   navBar.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".mobile-nav-link").forEach(link => link.addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navBar.classList.remove("active");
-}))
+document.querySelectorAll('.mobile-nav-link').forEach((link) => link.addEventListener("click", () => {
+  hamburger.classList.remove('active');
+  navBar.classList.remove('active');
+}));
